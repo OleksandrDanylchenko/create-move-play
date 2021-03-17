480cf4d72f59b2782d0d2c3f05276e4732412efa
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2021-03-17 22:10:19 +0200

    Added basic design for /intro screen

--- src/pages/Intro/index.tsx
+++ src/pages/Intro/index.tsx
@@ -1,11 +1,13 @@
 import React, { FunctionComponent } from 'react';
 import styles from './styles.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronCircleRight } from '@fortawesome/free-solid-svg-icons';
 
 const IntroView: FunctionComponent = () => {
   return (
     <div className={styles.container}>
       <article className={styles.info}>
-        <header>
+        <header className={styles.header}>
           <h1>
             Створюй.
             <br />
@@ -14,13 +16,16 @@ const IntroView: FunctionComponent = () => {
             Грай.
           </h1>
         </header>
-        <section>
+        <section className={styles.section}>
           <p>
             Приєднуйся до нас,
             <br />
             виконуй вправи та вигравай цінні призи
           </p>
         </section>
+        <button className={styles.joinButton}>
+          Приєднатися <FontAwesomeIcon icon={faChevronCircleRight} />
+        </button>
       </article>
     </div>
   );
